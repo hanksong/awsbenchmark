2288386f905e6376bd6ea179b8302c82c482bd10
Tim Rolsh <rolshud2@illinois.edu>
2025-04-29 18:33:43 -0500

    Add aws region names and fallback ami ids to global constants

--- Constants.py
+++ Constants.py
@@ -32,3 +32,49 @@ DEFAULT_CONFIG_OPTIONS = {
     "generate_visualizations": True,
     "generate_report": True,
 }
+
+# Region name mappings for human-readable output
+AWS_REGION_NAMES = {
+    "us-east-1": "virginia",
+    "us-east-2": "ohio",
+    "us-west-1": "california",
+    "us-west-2": "oregon",
+    "ca-central-1": "canada",
+    "eu-west-1": "ireland",
+    "eu-west-2": "london",
+    "eu-west-3": "paris",
+    "eu-central-1": "frankfurt",
+    "eu-north-1": "stockholm",
+    "ap-northeast-1": "tokyo",
+    "ap-northeast-2": "seoul",
+    "ap-northeast-3": "osaka",
+    "ap-southeast-1": "singapore",
+    "ap-southeast-2": "sydney",
+    "ap-south-1": "mumbai",
+    "sa-east-1": "saopaulo",
+    "af-south-1": "capetown",
+    "me-south-1": "bahrain"
+}
+
+# Fallback AMI IDs for Amazon Linux 2 in case API call fails
+AWS_FALLBACK_AMI_IDS = {
+    "us-east-1": "ami-0230bd60aa48260c6",
+    "us-east-2": "ami-06d6c38e6097eb144",
+    "us-west-1": "ami-079a2a9ac6ed793e6",
+    "us-west-2": "ami-09ee0d355a6091e77",
+    "ca-central-1": "ami-0bb3fae62b2968c22",
+    "eu-west-1": "ami-0cc7e92b19e61b6a4",
+    "eu-west-2": "ami-02e122a5b090f377d",
+    "eu-west-3": "ami-086ebba2a8fb34f1f",
+    "eu-central-1": "ami-05189af83ce42a6a7",
+    "eu-north-1": "ami-0bf30fc96c6ca89dc",
+    "ap-northeast-1": "ami-06ae089b59b645ddc",
+    "ap-northeast-2": "ami-0e4791176ea06b114",
+    "ap-northeast-3": "ami-0ede394b2c6f5af3c",
+    "ap-southeast-1": "ami-034f51cae2865c0e5",
+    "ap-southeast-2": "ami-0db6fa4ad94ed2496",
+    "ap-south-1": "ami-0a0d9cf81c5acd58c",
+    "sa-east-1": "ami-0fb4cf3a99aa89f49",
+    "af-south-1": "ami-028307eb8d34710b0",
+    "me-south-1": "ami-0255b97934a9bdc27"
+}
